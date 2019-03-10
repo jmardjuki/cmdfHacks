99cfd8c1be685fb9e5a4d596c4cdca53150968c0
ayuzou <imaizumi.ayumi@gmail.com>
2019-03-10 14:12:30 -0700

    transition from sad to happy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react';
-import logo from './images/neutral.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import IconDisplay from './IconDisplay.js';
 import ActionMenu from './ActionMenu.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      petImage: "sadnessPet",
+      emotion: "sad"
+    };
+    this.changeImage = this.changeImage.bind(this);
+  }
 
   changeImage() {
-
+    this.setState({
+      petImage: "happyPet",
+      emotion: "happy"
+    });
   }
 
   render() {
     return (
       <div className="App">
-          <div class="row">
-            <div class="col-2">
+          <div className="row">
+            <div class="col">
               <ActionMenu
                 value={5}
               />
             </div>
             <div class="col-8">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" onclick="this.changeImage()" />
-              </header>
+                <div className={this.state.petImage} onClick={this.changeImage} />
             </div>
-            <div class="col-2">
+            <div class="col">
               <IconDisplay 
                 content={"Affection"}
-                emotion={"Happy"}
+                emotion={this.state.emotion}
               />
             </div>
           </div>
--- src/IconDisplay.js
+++ src/IconDisplay.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class IconDisplay extends Component {
-
-	render() {
+function IconDisplay(props) {
 		const heartStyle = {
 			color: 'hotpink',
 			fontSize: '6em',
@@ -18,17 +15,26 @@ class IconDisplay extends Component {
 			marginRight: '20px'
 		};
 
+		var emotion;
+		if (props.emotion === "happy") {
+			emotion = <div>
+				<i className="fa fa-smile-o" style={faceStyle}/>
+			</div>;
+		}
+		else {
+			emotion = <div>
+				<i class="fa fa-frown-o" style={faceStyle} />
+			</div>;
+		}
+
 		return (
 			<div>
 				<div>
 					<i className="fa fa-heart-o" style={heartStyle}/>
 				</div>
-				<div>
-					<i className="fa fa-smile-o" style={faceStyle}/>
-				</div>
+				{emotion}
 			</div>
 		);
-	}
 }
 
 export default IconDisplay;
